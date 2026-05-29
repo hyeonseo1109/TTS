b3047108d79ca25c020797f552882c5ebddbbbf0
hyeonseo1109 <alsgustj2@naver.com>
2026-05-30 06:06:32 +0900

    fix: conf 수정

--- src-tauri/src/keyboard/mod.rs
+++ src-tauri/src/keyboard/mod.rs
@@ -1,18 +1,1 @@
 pub mod key_map;
-use tauri::{AppHandle, Emitter};
-
-use crate::keyboard::key_map::key_to_string;
-
-pub fn start_global_listener(app_handle: AppHandle) {
-    std::thread::spawn(move || {
-        let callback = move |event: Event| {
-            if let EventType::KeyPress(key) = event.event_type {
-                if let Some(k) = key_to_string(key) {
-                    let _ = app_handle.emit("global-key", k);
-                }
-            }
-        };
-
-        let _ = listen(callback);
-    });
-}

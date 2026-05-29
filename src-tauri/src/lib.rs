59aa3c9f6214202e1d3e25a20805882e0b992d5a
hyeonseo1109 <alsgustj2@naver.com>
2026-05-30 05:41:39 +0900

    fix: main.rs 수정

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,24 @@
+mod keyboard;
+
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+
+            let handle = app.handle().clone();
+            keyboard::start_global_listener(handle);
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod keyboard;
-
-use tauri::Manager;
-
 fn main() {
-    tauri::Builder::default()
-        .setup(|app| {
-            let handle = app.handle().clone();
-
-            keyboard::start_global_listener(handle);
-
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    app_lib::run();
 }
